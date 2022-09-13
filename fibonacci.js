b719385abb95e9fa9f91c2ccbd994ba2baac143d
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-09-13 11:29:50 +0100

    Tests passing for three nums

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = (num) => {
-  return [];
+  if (num === 0) {
+    return [];
+  } else {
+  return [0, 1, 1];
+  }
 };
 
 module.exports = fibonacci;
--- fibonacci.test.js
+++ fibonacci.test.js
@@ -4,4 +4,8 @@ describe("fibonacci", () => {
   it("takes the number zero and returns an empty array", () => {
     expect(fibonacci(0)).toEqual([]);
   });
+
+  it("takes the number three and returns an array with first three numbers of fibonacci sequence", () => {
+    expect(fibonacci(3)).toEqual([0, 1, 1]);
+  });
 });
