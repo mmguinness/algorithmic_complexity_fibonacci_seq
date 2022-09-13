59c9cd163f914882796a5eee99fb63a991233d67
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-09-13 11:47:53 +0100

    Tests for numbers 0 - 4

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,22 @@
 const fibonacci = (num) => {
+  const fibonacciSequence = [0];
   if (num === 0) {
     return [];
+  } else if (num === 1) {
+    return fibonacciSequence;
+  } else if (num === 2) {
+    fibonacciSequence.push(1);
+    return fibonacciSequence;
   } else {
-  return [0, 1, 1];
+    fibonacciSequence.push(1);
+
+    for (let i = 1; i < num - 1; i++) {
+      let nextNumberInSequence =
+        fibonacciSequence[i] + fibonacciSequence[i - 1];
+      console.log(nextNumberInSequence);
+      fibonacciSequence.push(nextNumberInSequence);
+    }
+    return fibonacciSequence;
   }
 };
 
--- fibonacci.test.js
+++ fibonacci.test.js
@@ -5,7 +5,19 @@ describe("fibonacci", () => {
     expect(fibonacci(0)).toEqual([]);
   });
 
+  it("takes the number one and returns an array with first number of fibonacci sequence", () => {
+    expect(fibonacci(1)).toEqual([0]);
+  });
+
+  it("takes the number two and returns an array with first two numbers of fibonacci sequence", () => {
+    expect(fibonacci(2)).toEqual([0, 1]);
+  });
+
   it("takes the number three and returns an array with first three numbers of fibonacci sequence", () => {
     expect(fibonacci(3)).toEqual([0, 1, 1]);
+  });
+
+  it("takes the number four and returns an array with first four numbers of fibonacci sequence", () => {
+    expect(fibonacci(4)).toEqual([0, 1, 1, 2]);
   });
 });
